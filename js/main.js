e2437b00a3f372c9cfa8cfb9b2c9e358d66ad29a
Dailyman <robin.lars.hakansson@gmail.com>
2017-03-20 13:18:20 +0100

    Added slide function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(function() {
+    $('#start_button').on('click', function(event) {
+        event.preventDefault();
 
+        $('#start-box').hide('slide',{direction:'left'},200);
+        $('html, body').css('overflowX', 'hidden');
+        window.setTimeout(function() {
+            $('#drink-box').show('slide',{direction:'right'},200,function() {
+                $('html, body').css('overflowX', 'auto');
+            });
+        }, 250);
+    });
+});
