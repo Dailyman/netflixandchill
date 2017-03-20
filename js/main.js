5ed2bc1a2ed1c386ec3f44635320ad16d48b8269
Dailyman <robin.lars.hakansson@gmail.com>
2017-03-20 14:10:14 +0100

    Added prev slide func

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    // --> NEXT -->
+
     // START -> DRINK
     $('#start_button').on('click', function(event) {
         event.preventDefault();
@@ -47,6 +49,61 @@ $(function() {
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
             $('#summary-box').show('slide',{direction:'right'},200,function() {
+                $('html, body').css('overflowX', 'auto');
+            });
+        }, 250);
+    });
+
+
+    // <-- PREVIOUS <--
+
+    // MOVIE <- SUMMARY
+    $('#summary-prev-button').on('click', function(event) {
+        event.preventDefault();
+
+        $('#summary-box').hide('slide',{direction:'right'},200);
+        $('html, body').css('overflowX', 'hidden');
+        window.setTimeout(function() {
+            $('#movie-box').show('slide',{direction:'left'},200,function() {
+                $('html, body').css('overflowX', 'auto');
+            });
+        }, 250);
+    });
+
+    // FOOD <- MOVIE
+    $('#movie-prev-button').on('click', function(event) {
+        event.preventDefault();
+
+        $('#movie-box').hide('slide',{direction:'right'},200);
+        $('html, body').css('overflowX', 'hidden');
+        window.setTimeout(function() {
+            $('#food-box').show('slide',{direction:'left'},200,function() {
+                $('html, body').css('overflowX', 'auto');
+            });
+        }, 250);
+    });
+
+    // DRINK <- FOOD
+    $('#food-prev-button').on('click', function(event) {
+        event.preventDefault();
+
+        $('#food-box').hide('slide',{direction:'right'},200);
+        $('html, body').css('overflowX', 'hidden');
+        window.setTimeout(function() {
+            $('#drink-box').show('slide',{direction:'left'},200,function() {
+                $('html, body').css('overflowX', 'auto');
+            });
+        }, 250);
+    });
+
+    // START <- DRINK
+    $('#drink-prev-button').on('click', function(event) {
+        event.preventDefault();
+
+        $('#drink-box').hide('slide',{direction:'right'},200);
+        $('html, body').css('overflowX', 'hidden');
+        window.setTimeout(function() {
+            $('#start-box').show('slide',{direction:'left'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
         }, 250);
