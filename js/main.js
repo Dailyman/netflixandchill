f992660c20b567ea768d65f2e6dcd6e9a5a60339
Dailyman <robin.lars.hakansson@gmail.com>
2017-03-20 13:36:26 +0100

    Added slides

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function() {
+
+    // START -> DRINK
     $('#start_button').on('click', function(event) {
         event.preventDefault();
 
@@ -6,6 +8,32 @@ $(function() {
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
             $('#drink-box').show('slide',{direction:'right'},200,function() {
+                $('html, body').css('overflowX', 'auto');
+            });
+        }, 250);
+    });
+
+    // DRINK -> MOVIE
+    $('#drink-button').on('click', function(event) {
+        event.preventDefault();
+
+        $('#drink-box').hide('slide',{direction:'left'},200);
+        $('html, body').css('overflowX', 'hidden');
+        window.setTimeout(function() {
+            $('#movie-box').show('slide',{direction:'right'},200,function() {
+                $('html, body').css('overflowX', 'auto');
+            });
+        }, 250);
+    });
+
+    // MOVIE -> SUMMARY
+    $('#movie-button').on('click', function(event) {
+        event.preventDefault();
+
+        $('#movie-box').hide('slide',{direction:'left'},200);
+        $('html, body').css('overflowX', 'hidden');
+        window.setTimeout(function() {
+            $('#summary-box').show('slide',{direction:'right'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
         }, 250);
