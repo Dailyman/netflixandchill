4db0e5436cd52c72dab2addea6246ad3a3f7ef72
jienna <jhedaker@gmail.com>
2017-03-22 00:09:59 +0100

    .

--- js/spoon.js
+++ js/spoon.js
@@ -1,27 +1,39 @@
-$("#getRandomFoodBtn").on("click", function(){
-    $("#food-container").empty();
+$(document).on("ready", function(){
+    $("#food-container").hide();
 
-    var type = $("#food-type-select").val();
-    var pref = $("#preference-type-select").val();
+    $("#getRandomFoodBtn").on("click", function(){
+        $("#food-container").empty();
 
-    var fUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1&tags=' + type + '%2C' + pref ;
+        var type = $("#food-type-select").val();
+        var pref = $("#preference-type-select").val();
 
-    $.ajax({
-        url: fUrl,
-        dateType: "JSON",
-        headers: {"X-Mashape-Authorization": "p7zkjTyp9Tmsh3Bk8Y7uF3eqJLcrp1O0x3PjsneZMi4XRFspNB"}
-    }).done(function(data){
-        console.log(data);
-        var results = data.recipes[0];
+        var fUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1&tags=' + type + '%2C' + pref ;
 
-        var title = results.title;
-        var image = results.image;
-        var sourceUrl = results.sourceUrl;
+        $.ajax({
+            url: fUrl,
+            dateType: "JSON",
+            headers: {"X-Mashape-Authorization": "p7zkjTyp9Tmsh3Bk8Y7uF3eqJLcrp1O0x3PjsneZMi4XRFspNB"}
+        }).done(function(data){
+            console.log(data);
+            var results = data.recipes[0];
+
+            var title = results.title;
+            var image = results.image;
+            var sourceUrl = results.sourceUrl;
         //for (var i = 0; i < results.length; i++);
         //    var stuff = results[i].;
+            /*image = '<img src="' + image + '" class="img-thumbnail">';
+            title = '<p>Titel: <span class="title">' + "<br>" + title + '</span></p>';
+            sourceUrl = '<a class="btn btn-primary btn-lg btn-block" target="_blank" href="' + sourceUrl + '" id="food-search-btn">Gå till recept</a>';*/
 
-        $("#food-container").append("<figure>" + '<img class="img-responsive" src="' + image + '">' + "</figure>");
-        $("#food-title").html(title);
+            //var addStuff = $('<div class="col-xs-12 col-md-6" centered>' + '<figure>' + image + '</figure>' + title + sourceUrl + '</div>');
+
+            $("#food-title").html(title);
+            $("#recipe-image").html('<img src="' + image + '" alt=some_text class="img-responsive" id="poster">');
+            //$("#food-container").append(addStuff);
+            //$("#food-container").html("<p>Title: " + title + "</p>");
+            //$("a#food-search-btn").attr("href", "sourceUrl");
+        });
+        $("#food-container").slideDown();
     });
-
 });
