40955f364c8a7696eec80a712e1b5073768ac3a1
Dailyman <robin.lars.hakansson@gmail.com>
2017-03-21 13:20:15 +0100

    nav

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 $(function() {
 
+    $('#page1').addClass('active-page');
+
     // --> NEXT -->
 
     // START -> DRINK
     $('#start_button').on('click', function(event) {
         event.preventDefault();
 
+        $('#page1').removeClass('active-page');
+
         $('#start-box').hide('slide',{direction:'left'},200);
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
+            $('#page2').addClass('active-page');
             $('#drink-box').show('slide',{direction:'right'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
@@ -19,9 +24,12 @@ $(function() {
     $('#drink-button').on('click', function(event) {
         event.preventDefault();
 
+        $('#page2').removeClass('active-page');
+
         $('#drink-box').hide('slide',{direction:'left'},200);
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
+            $('#page3').addClass('active-page');
             $('#food-box').show('slide',{direction:'right'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
@@ -32,9 +40,12 @@ $(function() {
     $('#food-button').on('click', function(event) {
         event.preventDefault();
 
+        $('#page3').removeClass('active-page');
+
         $('#food-box').hide('slide',{direction:'left'},200);
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
+            $('#page4').addClass('active-page');
             $('#movie-box').show('slide',{direction:'right'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
@@ -45,9 +56,12 @@ $(function() {
     $('#movie-button').on('click', function(event) {
         event.preventDefault();
 
+        $('#page4').removeClass('active-page');
+
         $('#movie-box').hide('slide',{direction:'left'},200);
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
+            $('#page5').addClass('active-page');
             $('#summary-box').show('slide',{direction:'right'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
@@ -60,10 +74,11 @@ $(function() {
     // MOVIE <- SUMMARY
     $('#summary-prev-button').on('click', function(event) {
         event.preventDefault();
-
+        $('#page5').removeClass('active-page');
         $('#summary-box').hide('slide',{direction:'right'},200);
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
+            $('#page4').addClass('active-page');
             $('#movie-box').show('slide',{direction:'left'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
@@ -73,10 +88,11 @@ $(function() {
     // FOOD <- MOVIE
     $('#movie-prev-button').on('click', function(event) {
         event.preventDefault();
-
+        $('#page4').removeClass('active-page');
         $('#movie-box').hide('slide',{direction:'right'},200);
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
+            $('#page3').addClass('active-page');
             $('#food-box').show('slide',{direction:'left'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
@@ -86,10 +102,11 @@ $(function() {
     // DRINK <- FOOD
     $('#food-prev-button').on('click', function(event) {
         event.preventDefault();
-
+        $('#page3').removeClass('active-page');
         $('#food-box').hide('slide',{direction:'right'},200);
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
+            $('#page2').addClass('active-page');
             $('#drink-box').show('slide',{direction:'left'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
@@ -99,10 +116,11 @@ $(function() {
     // START <- DRINK
     $('#drink-prev-button').on('click', function(event) {
         event.preventDefault();
-
+        $('#page2').removeClass('active-page');
         $('#drink-box').hide('slide',{direction:'right'},200);
         $('html, body').css('overflowX', 'hidden');
         window.setTimeout(function() {
+            $('#page1').addClass('active-page');
             $('#start-box').show('slide',{direction:'left'},200,function() {
                 $('html, body').css('overflowX', 'auto');
             });
