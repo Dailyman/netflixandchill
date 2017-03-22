24e95bafdfde35e3834d1fd360e9b4aa0c6b4af2
Oskar Katra <oskar1.katra@gmail.com>
2017-03-22 10:09:44 +0100

    Sum fix

--- js/randommovie.js
+++ js/randommovie.js
@@ -1,30 +1,38 @@
+ /*
+   Script för random-film.
+   Använder sig av Unogs-API.
+*/
 
+var $movie= $('.movieInfo'); //Hämtar elementet.
+$movie.hide(); //Döljer element.
 
-var $movie= $('#movieInfo');
-$movie.hide();
 
 $( "#get-random-movie-btn" ).click(function() {
-   var jsonData;
+   /*
+   Hämtar alla genre id från APIet.
+   Sorterar sedan ut id för den genre som användaren valt.
+   */
+   var $filmInfo= $('#filmInfo'); //Hämtar elementet.
+   $filmInfo.hide(); //Döljer element.
    var max=2017;
    var min=1970;
    var urlXX;
    randomSYear=Math.floor(Math.random() * (max - min + 1)) + min;
    randomEYear=randomSYear+2;
-   var selGenre=$("#movie-type-select").val(); // The value of the selected option
+   var selGenre=$("#movie-type-select").val();
    selGenre= selGenre.split(',');
 
-   //Hämta genre-id
+   //Ajax-anrop
    $.ajax({
       url: 'https://unogs-unogs-v1.p.mashape.com/api.cgi?t=genres', 
-      type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-      data: {}, // Additional parameters here
+      type: 'GET',
+      data: {},
       dataType: 'json',
       success: function(data) { console.dir((data.source)); },
       error: function(err) { alert(err); },
       beforeSend: function(xhr) {
-         xhr.setRequestHeader("X-Mashape-Authorization", "dd4HqXOyNRmsh069sHourYvxlvLqp1VqxVxjsnNFFHyUuYXBfj"); // Enter here your Mashape key
+         xhr.setRequestHeader("X-Mashape-Authorization", "dd4HqXOyNRmsh069sHourYvxlvLqp1VqxVxjsnNFFHyUuYXBfj"); //API-Nyckel
       },
-      // work with the response
       success: function (response) {
          genreId=response.ITEMS[selGenre[0]][selGenre[1]];
          selGenre=genreId.join("%2C");
@@ -33,43 +41,43 @@ $( "#get-random-movie-btn" ).click(function() {
       }
    });
 
-
-   //Hämta film från AP
    function randomMovie (){
+   /*
+   Funktion som hämtar filmer från API.
+   Film väljs sedan ut med hjälp av random
+   */
       $.ajax({
          url: 'https://unogs-unogs-v1.p.mashape.com'+urlXX, 
-         type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-         data: {}, // Additional parameters here
+         type: 'GET',
+         data: {},
          dataType: 'json',
          success: function(data) { console.dir((data.source)); },
          error: function(err) { alert(err); },
          beforeSend: function(xhr) {
-            xhr.setRequestHeader("X-Mashape-Authorization", "dd4HqXOyNRmsh069sHourYvxlvLqp1VqxVxjsnNFFHyUuYXBfj"); // Enter here your Mashape key
+            xhr.setRequestHeader("X-Mashape-Authorization", "dd4HqXOyNRmsh069sHourYvxlvLqp1VqxVxjsnNFFHyUuYXBfj"); //API-Nyckel.
          },
-
-         // work with the response
          success: function (response) {
-            count = response.COUNT; // Antal objekt i arrayen.
+            count = response.COUNT;
             if (count>100) {
                max=99;
             }
             else{
                max=count-1;
             }
-            min=0; //Sätter min.
-            random=Math.floor(Math.random() * (max - min + 1)) + min; //Random vilket objekt som ska väljas från svaret.
+            min=0;
+            random=Math.floor(Math.random() * (max - min + 1)) + min;
             title=response.ITEMS[random][1];
             year=response.ITEMS[random][7];
             posterlink=response.ITEMS[random][2];
             length=response.ITEMS[random][8];
             plot=response.ITEMS[random][3];
 
-            $("#title").html(title);
-            $("#year").html(year);
-            $("#length").html(length);
-            $("#plot").html(plot);
+            $(".title").html(title);
+            $(".year").html(year);
+            $(".length").html(length);
+            $(".plot").html(plot);
             $movie.show();
-            $("#posterbox").html("<img src="+posterlink+" alt=some_text class='img-responsive' id='poster'>");
+            $(".posterbox").html("<img src="+posterlink+" alt=some_text class='img-responsive' id='poster'>");
          }
 
       });
